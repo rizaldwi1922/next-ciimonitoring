1283db575b1edc970bf80b11c3303c3c07dae02c
RizalDwi <rizaldwi2708@gmail.com>
2023-03-28 21:54:09 +0700

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '103.190.28.23',
+      ref  : 'origin/main',
+      repo : 'git@github.com:rizaldwi1922/next-ciimonitoring.git',
+      path : '/home/next-ciimonitoring',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
